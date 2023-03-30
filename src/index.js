89b78bced3bcbda157de838b15e99db9197bd911
ValeriiHol <rielty@gmail.com>
2023-03-30 18:05:58 +0300

    install lodash.debounce notiflix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+// import { fetchCountries } from './fetch-countries';
 
 const DEBOUNCE_DELAY = 300;
