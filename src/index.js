ecd985ecbb05091b26e9e9a6318487948e9456a2
ValeriiHol <rielty@gmail.com>
2023-03-31 17:15:50 +0300

    ок

--- src/index.js
+++ src/index.js
@@ -12,5 +12,64 @@ const refs = {
 };
 const { countryInfo, countryList, searchBox } = refs;
 
-const UKR = fetchCountries(`ukraine`);
-console.dir(UKR);
+// const UKR = fetchCountries(`ukraine`);
+// console.dir(UKR);
+
+const clearRefs = () => {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+};
+
+const createCountriesListMarkup = data =>
+  data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">  ${name.official}</li>`
+    )
+    .join('');
+
+const createOneCountryMarkup = ([country]) => {
+  const { name, flags, capital, population, languages } = country;
+
+  return `<img src="${flags.png}" alt="${name.official}" width="200" height="100">
+  <h1>${name.official}</h1>
+  <p>Capital: ${capital}</p>
+  <p>Population: ${population}</p>
+  <p>Languages: ${Object.values(languages)}</p>`;
+};
+
+const checkData = data => {
+  if (data.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+  if (data.length > 1) {
+    countryList.innerHTML = createCountriesListMarkup(data);
+    return;
+  }
+  countryInfo.innerHTML = createOneCountryMarkup(data);
+};
+
+const showError = error =>
+  Notify.failure(
+    error.message === '404'
+      ? 'Oops, there is no country with that name'
+      : 'Oops, something went wrong'
+  );
+
+const findCountry = async name => {
+  try {
+    checkData(await fetchCountries(name));
+  } catch (error) {
+    showError(error);
+  }
+};
+
+const onInput = ({ target }) => {
+  const countryName = target.value.trim();
+  clearRefs();
+  if (!countryName) return;
+  findCountry(countryName);
+};
+
+searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
